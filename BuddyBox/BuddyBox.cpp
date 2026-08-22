0d976015aceabc3c675dbcf24f716166de199c32
Flimc <Flimc@FLIMLEW>
2026-08-21 23:44:56 -0600

    Add project files.

---
+++ BuddyBox/BuddyBox.cpp
@@ -0,0 +1,12 @@
+﻿// BuddyBox.cpp : Defines the entry point for the application.
+//
+
+#include "BuddyBox.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
